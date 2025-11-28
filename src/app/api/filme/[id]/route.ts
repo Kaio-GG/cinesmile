826ec32a9b5d625b942ea7da12d7b312bd94a16c
Kaio <kaiofdss@gmail.com>
2025-11-28 15:27:05 -0300

    Ajustes api

--- src/app/api/filme/[id]/route.ts
+++ src/app/api/filme/[id]/route.ts
@@ -1,26 +1,25 @@
 import { NextResponse } from "next/server";
 
 export async function GET(
-  _request: Request,
-  { params }: { params: any }
+  _req: Request,
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const id = params.id;
+  const { id } = await params;
 
   try {
-    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`, {
+      cache: "no-store",
+    });
 
     if (!res.ok) {
-      return NextResponse.json(
-        { error: "Erro ao buscar dados na API externa" },
-        { status: 500 }
-      );
+      return NextResponse.json({ error: "API TVMaze falhou" }, { status: 500 });
     }
 
     const data = await res.json();
     return NextResponse.json(data);
-  } catch (error) {
+  } catch (err) {
     return NextResponse.json(
-      { error: "Erro interno no servidor", details: String(error) },
+      { error: "Erro interno", details: String(err) },
       { status: 500 }
     );
   }
