16c9912e8e42d88e36b36d173622b19dd37e1ed7
Kaio <kaiofdss@gmail.com>
2025-11-28 12:54:33 -0300

    Ajuste api

--- src/app/api/episodios/[id]/route.ts
+++ src/app/api/episodios/[id]/route.ts
@@ -1,28 +1,28 @@
 import { NextResponse } from "next/server";
-
-
+import type { NextRequest } from "next/server";
 
 export async function GET(
-    _request: Request,
-    {params}: {params: {id: string}}
-    ) {
-    try{
-        console.log(params.id);
-        const res =  await fetch(`https://api.tvmaze.com/shows/${params.id}/episodes`);
+  _request: NextRequest,
+  context: { params: { id: string } }
+) {
+  const { id } = context.params;
 
-        if(!res.ok) {
-            return NextResponse.json(
-                {erro: "nao foi possivel buscar"},
-                {status: 500}
-            )
-        }
+  try {
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}/episodes`);
 
-        const data = await res.json();
-        return NextResponse.json(data);
-    }catch(erro){
-        return NextResponse.json(
+    if (!res.ok) {
+      return NextResponse.json(
+        { error: "não foi possível buscar episódios" },
+        { status: 500 }
+      );
+    }
+
+    const data = await res.json();
+    return NextResponse.json(data);
+  } catch (erro: any) {
+    return NextResponse.json(
       { error: "Erro interno no servidor", details: String(erro) },
       { status: 500 }
     );
-    }
+  }
 }
